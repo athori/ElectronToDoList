299123bf1571691bc7598ee5287b825ddcfcdb13
jstass <jasonstass@gmail.com>
2017-12-10 09:03:47 -0600

    Fourth Commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const electron = require('electron')
 const app = electron.app
-const BroserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow
 
-const Menu = electron.Menu;
+const Menu = electron.Menu
 
 //creates main window
 app.on('ready', _=>{
@@ -15,8 +15,47 @@ app.on('ready', _=>{
 
     const menu = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menu);
+
+    mainWindow.on('closed',_=>{
+        console.log('closed');
+        mainWindow = null;
+    })
 })//end main maindow
 
 const menuTemplate = [
+    {
+        label : electron.app.getName(),
+        submenu : [
+            {
+                label : 'quit',
+                click : _=>{
+                    app.quit()
+                }, 
+                accelerator: 'Ctrl+Q'
+            }
+        ]
+    },
+    {
+        label : 'File',
+        submenu : [
+            {
+                label : 'Save File'
+            },
+            {
+                type : 'separator'
+            },
+            {
+                label : 'Open File'
+            }
+        ]
+    },
+    {
+        label : "Add Item"
+    },
+    {
+        label : "Clear All Items"
+    },
+    {
 
+    }
 ]
